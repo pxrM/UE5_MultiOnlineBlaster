68ab4f719040a1e79cd95bf87d7af8374075386c
agamePC <agamePC@tencent.com>
2023-05-11 17:22:34 +0800

    创建自己的hud、playercontroller的c++类和蓝图类 并在GameMode里设置引用

---
+++ Blaster/Source/Blaster/HUD/BlasterHUD.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "BlasterHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ABlasterHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	virtual void DrawHUD() override;
+	
+};
