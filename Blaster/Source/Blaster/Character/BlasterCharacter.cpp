60a18e3eb8d196ff3c5b0d54fe0e53bce8d8daf8
pxrM <pxr680@163.com>
2023-07-11 11:29:13 +0800

    添加其它武器的更换弹夹动画

--- Blaster/Source/Blaster/Character/BlasterCharacter.cpp
+++ Blaster/Source/Blaster/Character/BlasterCharacter.cpp
@@ -511,22 +511,22 @@ void ABlasterCharacter::PlayReloadMagMontage()
 			SectionName = FName("Rifle");
 			break;
 		case EWeaponType::EWT_RocketLauncher:
-			SectionName = FName("Rifle");
+			SectionName = FName("RocketLauncher");
 			break;
 		case EWeaponType::EWT_Pistol:
-			SectionName = FName("Rifle");
+			SectionName = FName("Pistol");
 			break;
 		case EWeaponType::EWT_SubmachineGun:
-			SectionName = FName("Rifle");
+			SectionName = FName("Pistol");
 			break;
 		case EWeaponType::EWT_Shotgun:
-			SectionName = FName("Rifle");
+			SectionName = FName("Shotgun");
 			break;
 		case EWeaponType::EWT_SniperRifle:
-			SectionName = FName("Rifle");
+			SectionName = FName("SniperRifle");
 			break;
 		case EWeaponType::EWT_GrenadeLacuncher:
-			SectionName = FName("Rifle");
+			SectionName = FName("GrenadeLacuncher");
 			break;
 		}
 		AnimInstance->Montage_JumpToSection(SectionName);
