fb5fcd9f04c1d5a242a2484ac9dfbcc28ea1cbab
pxrM <pxr680@163.com>
2023-07-19 17:34:21 +0800

    创建血包拾取器及蓝图

---
+++ Blaster/Source/Blaster/Pickups/HealthPickup.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPickup.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Blaster/BlasterComponent/BuffComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+
+AHealthPickup::AHealthPickup()
+{
+	bReplicates = true;
+
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+	PickupEffectComponent->SetupAttachment(RootComponent);
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+	if (BlasterCharacter)
+	{
+
+	}
+	Destroy();
+}
+
+void AHealthPickup::Destroyed()
+{
+	if (PickupDesEffect)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, PickupDesEffect, GetActorLocation(), GetActorRotation());
+	}
+
+	Super::Destroyed();
+}
---
+++ Blaster/Source/Blaster/Pickups/HealthPickup.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "HealthPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AHealthPickup : public APickup
+{
+	GENERATED_BODY()
+
+public:
+	AHealthPickup();
+	virtual void Destroyed() override;
+
+protected:
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	) override;
+
+
+public:
+	UPROPERTY(EditAnywhere)
+		float HealAmount = 100.f; //治疗量
+	UPROPERTY(EditAnywhere)
+		float HealingTime = 5.f; //治疗所需时间
+
+	UPROPERTY(VisibleAnywhere)
+		class UNiagaraComponent* PickupEffectComponent; //场景展示特效
+	UPROPERTY(EditAnywhere)
+		class UNiagaraSystem* PickupDesEffect; //销毁时产生的特效
+	
+};
