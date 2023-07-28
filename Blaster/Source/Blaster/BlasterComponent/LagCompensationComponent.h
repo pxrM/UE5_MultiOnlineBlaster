fef251baaaeb062ced231ca720408c9c87d8d944
pxrM <pxr680@163.com>
2023-07-28 17:32:04 +0800

    服务器倒带：创建桢数据结构 1

--- Blaster/Source/Blaster/BlasterComponent/LagCompensationComponent.h
+++ Blaster/Source/Blaster/BlasterComponent/LagCompensationComponent.h
@@ -10,12 +10,54 @@
 #include "LagCompensationComponent.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+/// <summary>
+/// 角色桢数据
+/// </summary>
+USTRUCT(BlueprintType)
+struct FFramePackage
+{
+	GENERATED_BODY()
+
+		/// <summary>
+		/// 存储时间
+		/// </summary>
+		UPROPERTY()
+		float Time;
+	UPROPERTY()
+		TMap<FName, FBoxInformation> HitBoxInfo;
+};
+
+/// <summary>
+/// 由于不能直接存box的指针，因为指针指向的是地址，所以需要单独的数据结构存储盒子的信息
+/// </summary>
+USTRUCT(BlueprintType)
+struct FBoxInformation
+{
+	GENERATED_BODY()
+
+		/// <summary>
+		/// 位置
+		/// </summary>
+		UPROPERTY()
+		FVector Location;
+	/// <summary>
+	/// 旋转
+	/// </summary>
+	UPROPERTY()
+		FRotator Rotation;
+	/// <summary>
+	/// 盒子范围
+	/// </summary>
+	UPROPERTY()
+		FVector BoxExtent;
+};
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BLASTER_API ULagCompensationComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	ULagCompensationComponent();
 	// Called every frame
@@ -25,8 +67,9 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
+
+public:
 
 
-		
+
 };
