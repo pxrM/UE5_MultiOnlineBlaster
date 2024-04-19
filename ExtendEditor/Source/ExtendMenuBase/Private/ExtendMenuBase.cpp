2a062efa9328cd1b85763114104923a920c78eb3
pxrM <pxr680@163.com>
2024-04-19 15:44:17 +0800

    编辑器拓展 ContentBrowser右键菜单

--- ExtendEditor/Source/ExtendMenuBase/Private/ExtendMenuBase.cpp
+++ ExtendEditor/Source/ExtendMenuBase/Private/ExtendMenuBase.cpp
@@ -4,6 +4,7 @@
 
 #include "ExtendMenuBase.h"
 #include "LevelEditor.h"
+#include "ContentBrowserModule.h"
 
 IMPLEMENT_GAME_MODULE(FExtendMenuBase, ExtendMenuBase)
 
@@ -12,6 +13,7 @@ void FExtendMenuBase::StartupModule()
 	IModuleInterface::StartupModule();
 
 	ExtendMenuByFExtend();
+	ExtendContentBrowserByFExtend();
 }
 
 void FExtendMenuBase::ShutdownModule()
@@ -27,8 +29,8 @@ void FExtendMenuBase::ExtendMenuByFExtend()
 	* 将需要拓展的内容设置到该对象中。
 	* 将该对象添加到由 FExtensibilityManager 管理的 Extenders 中。
 	*/
-	
-	//主菜单按钮下的子按钮
+
+	// 主菜单按钮下的子按钮
 	TSharedPtr<FExtender> MainMenuExtender = MakeShared<FExtender>();
 	MainMenuExtender->AddMenuExtension(
 		// 用于定位的ExtensionHook（扩展挂钩，编辑器扩展中用于添加自定义功能或修改编辑器行为的挂钩点。通过扩展挂钩，开发者可以在编辑器中注入自定义逻辑，从而实现对编辑器的定制和增强。）
@@ -38,12 +40,20 @@ void FExtendMenuBase::ExtendMenuByFExtend()
 		FMenuExtensionDelegate::CreateRaw(this, &FExtendMenuBase::MakeExistingMainMenuEntry)// 绑定制作按钮的代理函数
 	);
 
-
+	// 主菜单按钮
 	MainMenuExtender->AddMenuBarExtension(
 		"Help",
 		EExtensionHook::After,
 		nullptr,
 		FMenuBarExtensionDelegate::CreateRaw(this, &FExtendMenuBase::MakeNewMenuBarEntry)
+	);
+
+	// ToolBar上的按钮
+	MainMenuExtender->AddToolBarExtension(
+		"ProjectSettings",
+		EExtensionHook::After,
+		nullptr,
+		FToolBarExtensionDelegate::CreateRaw(this, &FExtendMenuBase::MakeToolBarEntry)
 	);
 
 	FLevelEditorModule& LevelEditorModule = FModuleManager::Get().LoadModuleChecked<FLevelEditorModule>("LevelEditor");
@@ -61,7 +71,7 @@ void FExtendMenuBase::MakeExistingMainMenuEntry(FMenuBuilder& MenuBuilder)
 		FText::FromString("This is a existing hook extend"),// 按钮提示
 		FSlateIcon(), // 我们可以为按钮添加图标，这里暂时缺省
 		FUIAction(FExecuteAction::CreateRaw(this, &FExtendMenuBase::ExistingMainMenuEntryAction))// 绑定执行按钮的代理函数
-		);
+	);
 }
 
 void FExtendMenuBase::ExistingMainMenuEntryAction()
@@ -95,4 +105,60 @@ void FExtendMenuBase::MakeNewMainMenuEntry(FMenuBuilder& MenuBuilder)
 void FExtendMenuBase::NewMainMenuEntryAction()
 {
 	UE_LOG(LogTemp, Warning, TEXT("NewMainMenuEntryAction is called."));
+}
+
+void FExtendMenuBase::MakeToolBarEntry(FToolBarBuilder& ToolBarBuilder)
+{
+	ToolBarBuilder.AddToolBarButton(FUIAction(FExecuteAction::CreateRaw(this, &FExtendMenuBase::ToolBarEntryAction)));
+}
+
+void FExtendMenuBase::ToolBarEntryAction()
+{
+	UE_LOG(LogTemp, Warning, TEXT("ToolBarEntryAction is called."));
+}
+
+void FExtendMenuBase::ExtendContentBrowserByFExtend()
+{
+	/**
+	 * 加载 FContentBrowserModule 模块。
+	 通过 GetAll***Extenders() 函数获取 ContentBrowser 对应部分的 Extenders。
+	 注入自己的 FExtender 到 Extenders。
+	 */
+
+	 // 新的FContentBrowserModule模块
+	FContentBrowserModule& ContentBrowserModule = FModuleManager::Get().LoadModuleChecked<FContentBrowserModule>("ContentBrowser");
+	// 通过GetAllAssetContextMenuExtenders获取Extenders
+	TArray<FContentBrowserMenuExtender_SelectedPaths>& Extenders = ContentBrowserModule.GetAllAssetContextMenuExtenders();
+	// 注意此处添加代理应与Extenders中对应
+	Extenders.Add(FContentBrowserMenuExtender_SelectedPaths::CreateRaw(this, &FExtendMenuBase::ExtendAssetContextMenu));
+}
+
+TSharedRef<FExtender> FExtendMenuBase::ExtendAssetContextMenu(const TArray<FString>& SelectedPaths)
+{
+	TSharedRef<FExtender> MenuExtender(new FExtender());
+	if (SelectedPaths.Num() > 0)
+	{
+		MenuExtender->AddMenuExtension(
+			"ContentBrowserGetContent",
+			EExtensionHook::Before,
+			nullptr,
+			FMenuExtensionDelegate::CreateRaw(this, &FExtendMenuBase::MakeAssetContextMenuEntry)
+		);
+	}
+	return MenuExtender;
+}
+
+void FExtendMenuBase::MakeAssetContextMenuEntry(FMenuBuilder& MenuBuilder)
+{
+	MenuBuilder.AddMenuEntry(
+		FText::FromString("Asset Context Menu Button"),
+		FText::FromString("This is a asset context menu button"),
+		FSlateIcon(),
+		FUIAction(FExecuteAction::CreateRaw(this, &FExtendMenuBase::AssetContextMenuEntryAction))
+	);
+}
+
+void FExtendMenuBase::AssetContextMenuEntryAction()
+{
+	UE_LOG(LogTemp, Warning, TEXT("AssetContextMenuEntryAction is called."));
 }
--- ExtendEditor/Source/ExtendMenuBase/Public/ExtendMenuBase.h
+++ ExtendEditor/Source/ExtendMenuBase/Public/ExtendMenuBase.h
@@ -28,4 +28,21 @@ private:
 	/** 在新拓展点生成菜单按钮的调用事件 */
 	void NewMainMenuEntryAction();
 
+	/** 生成一个ToolBar按钮 */
+	void MakeToolBarEntry(FToolBarBuilder& ToolBarBuilder);
+	/** 在ToolBar按钮点击时触发事件 */
+	void ToolBarEntryAction();
+
+
+
+	/** FExtend拓展ContentBrowser入口函数 */
+	void ExtendContentBrowserByFExtend();
+
+	/** 拓展资产面板上下文菜单 */
+	TSharedRef<FExtender> ExtendAssetContextMenu(const TArray<FString>& SelectedPaths);
+	/** 生成一个资产面板上下文菜单按钮 */
+	void MakeAssetContextMenuEntry(FMenuBuilder& MenuBuilder);
+	/** 新资产面板上下文菜单按钮触发事件 */
+	void AssetContextMenuEntryAction();
+
 };
