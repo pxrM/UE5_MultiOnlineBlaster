bf51cb83929078882c815785e7151b725dab0cc7
pxrM <pxr680@163.com>
2024-05-22 16:04:54 +0800

    GAS：使用Effect的Period使属性持续改变

--- Aura/Source/Aura/Public/Actor/AuraEffectActor.h
+++ Aura/Source/Aura/Public/Actor/AuraEffectActor.h
@@ -8,16 +8,21 @@
 
 
 class UGameplayEffect;
+
 /*
- * GameplayEffect简称GE，它是技能Buff、被动技能、技能伤害等各种游戏效果的抽象。
- * 通用actor，应用gas某种GE对属性做出改变
+ * 通用 actor，应用gas某种GE对属性做出改变（GameplayEffect简称GE，它是技能Buff、被动技能、技能伤害等各种游戏效果的抽象。）
+ *
+ * GameplayEffects通过Modifiers 和Executions (GameplayEffectExecutionCalculation) 改变Attributes。
  * 
  * UGameplayEffect类中的 DurationPolicy 属性用于控制游戏效果的持续时间策略。
- * 这个属性确定了游戏效果是瞬时的（instant）、永久的（infinite）、或者有限持续时间的。
+ * 这个属性确定了游戏效果是立即的（instant）、永久的（infinite）、或者有限持续时间的。
  * DurationPolicy 属性有三种可能的取值：
  *		Instant: 表示游戏效果是瞬时的，一旦应用即立即生效，然后立刻结束。
  *		Infinite: 表示游戏效果是永久的，会一直持续下去，直到被移除或者取消。
- *		Timed Duration（有限持续时间）: 表示游戏效果有一个预定的持续时间，在这段时间内效果会持续生效，之后效果会自动失效结束。
+ *		Duration（有限持续时间）: 表示游戏效果有一个预定的持续时间，在这段时间内效果会持续生效，之后效果会自动失效结束。
+ *	 Periodic：
+ *		Duration和Infinite 有 Periodic（周期效果）配置项，可以通过配置 Period 每隔x秒周期性的执行 Modifiers 和 Executions。
+ *		周期性效果可以看作是 Instant Effects，每次修改属性的 BaseValue 并且执行 GameplayCues。这对实现持续伤害效果非常有用。
  */
 UCLASS()
 class AURA_API AAuraEffectActor : public AActor
