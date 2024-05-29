9b8c250d4bab33ce2fe4fadb1eb37c07c41d762b
pxrM <pxr680@163.com>
2024-05-29 14:55:34 +0800

    GAS：在ge蓝图中添加tag，并在Apply委托函数中获取这些tag

--- Aura/Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Aura/Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -6,11 +6,22 @@
 void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
 	// 这个委托用于在Effect应用到自身角色时触发相应的逻辑
-	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, UAuraAbilitySystemComponent::EffectApplied);
+	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
 }
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
                                                 const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("Effect Applied"));
+	// GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("Effect Applied"));
+
+	// 获取所有标签并填充到容器中
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+
+	for(const FGameplayTag& Tag : TagContainer)
+	{
+		// TODO: Broadcast the tag to the widget controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+	}
 }
