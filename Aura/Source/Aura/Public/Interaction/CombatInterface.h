d95ea51c854214c7c51051b44ee135e75c1aa43e
pxrM <pxr680@163.com>
2024-06-20 20:27:54 +0800

    GAS：添加MotionWarping插件，使角色在释放法术时能面向施法方向

--- Aura/Source/Aura/Public/Interaction/CombatInterface.h
+++ Aura/Source/Aura/Public/Interaction/CombatInterface.h
@@ -7,7 +7,7 @@
 #include "CombatInterface.generated.h"
 
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, BlueprintType)
 class UCombatInterface : public UInterface
 {
 	GENERATED_BODY()
@@ -26,4 +26,9 @@ public:
 	virtual int32 GetPlayerLevel();
 	// 获取武器上的插槽位置
 	virtual FVector GetCombatSocketLocation();
+
+	// 更新角色面向攻击目标的方向
+	// BlueprintImplementableEvent：具体实现由蓝图来决定，而不是在C++中直接实现
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void UpdateFaceingTarget(const FVector& Target);
 };
