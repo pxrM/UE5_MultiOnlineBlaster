ac3675b628dc74487d3bd538d40b29a7e033e200
pxrM <pxr680@163.com>
2024-06-21 14:58:50 +0800

    GAS：添加火球术的音效和撞击动效

--- Aura/Source/Aura/Private/Actor/AuraProjectileActor.cpp
+++ Aura/Source/Aura/Private/Actor/AuraProjectileActor.cpp
@@ -5,6 +5,9 @@
 
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
 
 AAuraProjectileActor::AAuraProjectileActor()
 {
@@ -29,12 +32,43 @@ void AAuraProjectileActor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	SetLifeSpan(LifeSpan); // 设置该 Actor 的生命周期时间。当生命周期到期时，Actor 会自动销毁。
+
 	SphereCmp->OnComponentBeginOverlap.AddDynamic(this, &AAuraProjectileActor::OnSphereOverlap);
+
+	LoopingSoundCmp = UGameplayStatics::SpawnSoundAttached(LoopingSound, GetRootComponent());
+}
+
+void AAuraProjectileActor::Destroyed()
+{
+	// 证明当前没有触发Overlap事件，在销毁前播放击中特效
+	if (!bHit && !HasAuthority())
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+		LoopingSoundCmp->Stop();
+	}
+	Super::Destroyed();
 }
 
 void AAuraProjectileActor::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+                                           UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+	LoopingSoundCmp->Stop();
+	
+	if(HasAuthority())
+	{
+		// Server
+		Destroy();
+	}
+	else
+	{
+		// Client
+		// 防止客户端在被销毁之前发生重叠（网络问题）
+		bHit = true;
+	}
 }
 
 
--- Aura/Source/Aura/Public/Actor/AuraProjectileActor.h
+++ Aura/Source/Aura/Public/Actor/AuraProjectileActor.h
@@ -8,6 +8,7 @@
 
 class UProjectileMovementComponent;
 class USphereComponent;
+class UNiagaraSystem;
 
 UCLASS()
 class AURA_API AAuraProjectileActor : public AActor
@@ -20,6 +21,7 @@ public:
 	
 protected:
 	virtual void BeginPlay() override;
+	virtual void Destroyed() override;
 	
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
@@ -29,9 +31,29 @@ private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> SphereCmp;
 
+	UPROPERTY(EditDefaultsOnly)
+	float LifeSpan = 15.f;
+	
+	bool bHit = false;
+	
+	// 碰撞后的特效
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UNiagaraSystem> ImpactEffect;
+
+	// 碰撞后的音效
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundBase> ImpactSound;
+
+	// 
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundBase> LoopingSound;
+	UPROPERTY()
+	TObjectPtr<UAudioComponent> LoopingSoundCmp;
+	
 
 public:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UProjectileMovementComponent> ProjectileMovementCmp;
 
 };
+ 
