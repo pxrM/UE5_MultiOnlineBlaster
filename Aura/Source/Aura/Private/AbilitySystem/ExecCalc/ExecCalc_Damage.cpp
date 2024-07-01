bc417fb684b38d775ce237a73657846780c7b340
pxrM <pxr680@163.com>
2024-07-01 17:11:14 +0800

    GAS：基于Execution Calculations创建伤害计算类（结合暴击属性等进行计算）

--- Aura/Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Aura/Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -4,10 +4,36 @@
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+// 创建一个结构用来保存所有需要捕获的属性
+
+struct AuraDamageStatics
+{
+	/* 声明对目标的属性获取 start */
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	
+	/* 声明对目标的属性获取 end */
+	
+	AuraDamageStatics()
+	{
+		// 指定的类 S 中执行属性捕获的定义操作，将捕获的属性和相关定义存储起来。
+		// 参数：S.属性集 P.属性名 T.目标还是自身 B.是否设置快照（true为创建时获取属性，false为应用时获取属性）
+		// 这里获取受击目标的属性值
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+	}
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics DStatic;
+	return DStatic;
+}
 
 UExecCalc_Damage::UExecCalc_Damage()
 {
-	
+	// 添加到捕获列表里
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -20,4 +46,21 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
 
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	// 用于在 Aggregator（聚合器）中评估属性值时传递参数。聚合器在游戏中用于计算和管理属性（Attributes）的变化和影响，比如生命值、攻击力等
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+	// 在执行计算中进行实际捕获
+	float Armor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
+	Armor = FMath::Max<float>(0.f, Armor);
+	// 创建一个游戏修改器的评估数据对象，用于描述要应用的修改器的效果。
+	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	// 将 EvaluatedData 添加到输出执行结果中
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
+	
--- Aura/Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
+++ Aura/Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -7,15 +7,20 @@
 #include "ExecCalc_Damage.generated.h"
 
 /**
- * Execution Calculations是UE中GameplayEffects系统的一部分，用于在GameplayEffect执行期间进行自定义的计算和逻辑操作。它允许开发者根据特定的游戏需求，灵活地处理和修改游戏中的属性（Attributes）。
- *  功能强大且灵活：与ModifierMagnitudeCalculation类似，ExecCalc可以捕获Attribute，并可选择性地为其创建Snapshot。与MMC不同的是，ExecCalc 可以修改多个Attribute，基本上可以处理程序员想要做的任何事。
+ * Execution Calculations是UE中GameplayEffects系统的一部分，用于在GameplayEffect执行期间进行自定义的计算和逻辑操作。
+ *  它允许开发者根据特定的游戏需求，灵活地处理和修改游戏中的属性（Attributes）。
+ *  功能强大且灵活：与ModifierMagnitudeCalculation类似，ExecCalc可以捕获Attribute，并可选择性地为其创建Snapshot。
+ *				  与MMC不同的是，ExecCalc 可以修改多个Attribute，基本上可以处理程序员想要做的任何事。
  *  不可预测性：这种强大和灵活性的代价是它是不可预测的（Modifiers是可以预测的），且必须在C++中实现。这是因为ExecCalc提供了很高的自由度，允许开发者根据特定的游戏逻辑进行自定义计算。
- *  使用场景：ExecCalc最普遍的应用场景是计算一个来自很多源（Source）和目标（Target）中Attribute伤害值的复杂公式。例如，在计算伤害时，可以考虑攻击者的攻击力、防御者的防御力、各种增益和减益效果等多个因素。
- *  限制：需要注意的是，ExecCalc只能由即刻（Instant）和周期性（Periodic）Gameplay Effect使用。功能强大也带来了劣势就是它不支持预测（Prediction）。属性获取时不会运行PreAttributeChange函数，
- *		所以，我们还需要在其内部实现一遍属性限制。只能够在服务器运行并且在Net Execution Policies设置为 Local Predicted，Server Initiated和Server Only时。
+ *  使用场景：ExecCalc最普遍的应用场景是计算一个来自很多源（Source）和目标（Target）中Attribute伤害值的复杂公式。
+ *			 例如，在计算伤害时，可以考虑攻击者的攻击力、防御者的防御力、各种增益和减益效果等多个因素。
+ *  限制：需要注意的是，ExecCalc只能由即刻（Instant）和周期性（Periodic）Gameplay Effect使用。
+ *		 功能强大也带来了劣势就是它不支持预测（Prediction）。属性获取时不会运行PreAttributeChange函数，
+ *		 所以，我们还需要在其内部实现一遍属性限制。只能够在服务器运行并且在Net Execution Policies设置为 Local Predicted，Server Initiated和Server Only时。
  *
  *  它和GameplayModMagnitudeCalculation很像，但是可以一次性修改多个属性。它不是只返回一个值，而是直接在函数内部对属性进行了修改。.
- *  关于快照 Snapshoting，如果选择了它，快照的属性值将会在GE的Spec创建的时候设置，如果不选择，那它将会在应用GE时去获取属性值。注意，即使选择快照，目标的属性也是在应用GE时获取，没应用之前我们也不知道目标是谁。
+ *  关于快照 Snapshoting，如果选择了它，快照的属性值将会在GE的Spec创建的时候设置，如果不选择，那它将会在应用GE时去获取属性值。
+ *  注意，即使选择快照，目标的属性也是在应用GE时获取，没应用之前我们也不知道目标是谁。
  */
 UCLASS()
 class AURA_API UExecCalc_Damage : public UGameplayEffectExecutionCalculation
