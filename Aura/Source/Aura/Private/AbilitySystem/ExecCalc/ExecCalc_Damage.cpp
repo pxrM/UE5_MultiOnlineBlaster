3813018b05dca58159401bf678050a5f654e7fc3
pxrM <pxr680@163.com>
2024-07-02 19:14:42 +0800

    GAS：将格挡几率应用到执行计算中

--- Aura/Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Aura/Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -4,6 +4,7 @@
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
 // 创建一个结构用来保存所有需要捕获的属性
@@ -12,7 +13,7 @@ struct AuraDamageStatics
 {
 	/* 声明对目标的属性获取 start */
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
-	
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 	/* 声明对目标的属性获取 end */
 	
 	AuraDamageStatics()
@@ -21,6 +22,7 @@ struct AuraDamageStatics
 		// 参数：S.属性集 P.属性名 T.目标还是自身 B.是否设置快照（true为创建时获取属性，false为应用时获取属性）
 		// 这里获取受击目标的属性值
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
 	}
 };
 
@@ -34,6 +36,7 @@ UExecCalc_Damage::UExecCalc_Damage()
 {
 	// 添加到捕获列表里
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -54,13 +57,29 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	FAggregatorEvaluateParameters EvaluateParameters;
 	EvaluateParameters.SourceTags = SourceTags;
 	EvaluateParameters.TargetTags = TargetTags;
+
+	// 获取由 Caller Magnitude 设置的伤害 
+	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
+	// 获取目标身上的格挡几率，并确定是否成功格挡，如果格挡成功，伤害会减少
+	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluateParameters, TargetBlockChance);
+	TargetBlockChance = FMath::Max<float>(0.f, TargetBlockChance);
+	const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+	Damage = bBlocked ? Damage / 2.f : Damage;
+
+	// 将计算结果写入输出
+	const FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
+	/*
 	// 在执行计算中进行实际捕获
 	float Armor = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
+	++Armor;
 	Armor = FMath::Max<float>(0.f, Armor);
 	// 创建一个游戏修改器的评估数据对象，用于描述要应用的修改器的效果。
 	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
 	// 将 EvaluatedData 添加到输出执行结果中
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
+	*/ 
 }
 	
