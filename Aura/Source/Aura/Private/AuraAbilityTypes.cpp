b5dde76911821a3ccd2a88f8a7cd66a9c6364c3f
pxrM <pxr680@163.com>
2024-07-05 19:41:36 +0800

    GAS: 重写覆盖FGameplayEffectContext的序列化函数，并添加自己的序列化内容

--- Aura/Source/Aura/Private/AuraAbilityTypes.cpp
+++ Aura/Source/Aura/Private/AuraAbilityTypes.cpp
@@ -1,7 +1,116 @@
 ﻿
 #include "AuraAbilityTypes.h"
 
+
 bool FAuraGameplayEffectContent::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
 {
+	int32 RepBits = 0;
+
+	// 如果当前是保存操作 
+	if(Ar.IsSaving())
+	{
+		if (bReplicateInstigator && Instigator.IsValid())
+		{
+			RepBits |= 1 << 0;
+		}
+		if (bReplicateEffectCauser && EffectCauser.IsValid() )
+		{
+			RepBits |= 1 << 1;
+		}
+		if (AbilityCDO.IsValid())
+		{
+			RepBits |= 1 << 2;
+		}
+		if (bReplicateSourceObject && SourceObject.IsValid())
+		{
+			RepBits |= 1 << 3;
+		}
+		if (Actors.Num() > 0)
+		{
+			RepBits |= 1 << 4;
+		}
+		if (HitResult.IsValid())
+		{
+			RepBits |= 1 << 5;
+		}
+		if (bHasWorldOrigin)
+		{
+			RepBits |= 1 << 6;
+		}
+
+		/* Add start */
+		if(bIsBlockedHit)
+		{
+			RepBits |= 1 << 7;
+		}
+		if(bIsCriticalHit)
+		{
+			RepBits |= 1 << 8;
+		}
+		/* Add end */
+	}
+
+	Ar.SerializeBits(&RepBits, 9);
+
+	if (RepBits & (1 << 0))
+	{
+		Ar << Instigator;
+	}
+	if (RepBits & (1 << 1))
+	{
+		Ar << EffectCauser;
+	}
+	if (RepBits & (1 << 2))
+	{
+		Ar << AbilityCDO;
+	}
+	if (RepBits & (1 << 3))
+	{
+		Ar << SourceObject;
+	}
+	if (RepBits & (1 << 4))
+	{
+		SafeNetSerializeTArray_Default<31>(Ar, Actors);
+	}
+	if (RepBits & (1 << 5))
+	{
+		if (Ar.IsLoading())
+		{
+			if (!HitResult.IsValid())
+			{
+				HitResult = TSharedPtr<FHitResult>(new FHitResult());
+			}
+		}
+		HitResult->NetSerialize(Ar, Map, bOutSuccess);
+	}
+	if (RepBits & (1 << 6))
+	{
+		Ar << WorldOrigin;
+		bHasWorldOrigin = true;
+	}
+	else
+	{
+		bHasWorldOrigin = false;
+	}
+
+	/* Add start */
+	if(RepBits & (1 << 7))
+	{
+		Ar << bIsBlockedHit;
+	}
+	if(RepBits & (1 << 8))
+	{
+		Ar << bIsCriticalHit;
+	}
+	/* Add end */
+
+	// 如果当前是加载操作，表示正在从网络或存储设备中加载数据到内存中
+	if (Ar.IsLoading())
+	{
+		AddInstigator(Instigator.Get(), EffectCauser.Get()); // Just to initialize InstigatorAbilitySystemComponent
+	}	
+	
+	bOutSuccess = true;
+	
 	return true;
 }
--- Aura/Source/Aura/Public/AuraAbilityTypes.h
+++ Aura/Source/Aura/Public/AuraAbilityTypes.h
@@ -17,15 +17,20 @@ public:
 		return FGameplayEffectContext::StaticStruct();
 	}
 
-	// 覆写用于序列化配置的函数（确认该结构体的序列化方式），将对应的参数保存，这样在服务器端，可以完整的复原这个实例。
+	// 覆写用于序列化配置的函数（确认该结构体的网络序列化方式），将对应的参数保存，这样在服务器端，可以完整的复原这个实例。
+	// FArchive& Ar: 是一个文件存档对象的引用，用于实际的序列化和反序列化操作。
+	//				Ar.SerializeBits(&RepBits, 7); 告诉这个地址的数据前七位数据有效
+	// UPackageMap* Map: 是一个包映射对象的指针（将对象映射到网络通信数据索引），用于处理包含的对象序列化。
+	// bool& bOutSuccess: 一个输出参数，用于指示序列化是否成功完成。
 	virtual bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
 
 	bool GetIsCriticalHit() const { return bIsCriticalHit; }
 	bool GetIsBlockedHit() const { return bIsBlockedHit; }
 
 	void SetIsCriticalHit(const bool bInCriticalHit) { bIsCriticalHit = bInCriticalHit; }
-	void SetIsBlockedHit(const bool bInBlockedHit) { bIsCriticalHit = bInBlockedHit; }
+	void SetIsBlockedHit(const bool bInBlockedHit) { bIsBlockedHit = bInBlockedHit; }
 
+	
 protected:
 	// 是否成功格挡
 	UPROPERTY()
