3b7d5cabc0499cf2d6661c06c5c47e2811e4be16
pxrM <49084047+pxrM@users.noreply.github.com>
2025-06-04 23:43:45 +0800

    Aura-p-4

--- Aura/Source/Aura/Private/UI/Widget/PhotoWidget.cpp
+++ Aura/Source/Aura/Private/UI/Widget/PhotoWidget.cpp
@@ -30,6 +30,20 @@ FEventReply UPhotoWidget::TouchMoved(FGeometry MyGeometry, const FPointerEvent& 
 		SelectionEnd = MyGeometry.AbsoluteToLocal(InTouchEvent.GetScreenSpacePosition());
 		// 计算选择框的大小
 		FVector2D BoxSize = FVector2D(FMath::Abs(SelectionEnd.X - SelectionStart.X), FMath::Abs(SelectionEnd.Y - SelectionStart.Y));
+		// 计算选择框的左上角位置
+		FVector2D BoxLeftTop = FVector2D(
+			FMath::Min(SelectionStart.X, SelectionEnd.X),
+			FMath::Min(SelectionStart.Y, SelectionEnd.Y)
+		);
+		// 获取小部件的本地大小
+		FVector2D WidgetSize = MyGeometry.GetLocalSize();
+		// 计算选择框的中心点（中心点坐标可以表示为：中心点x = 左上角x + 宽度/2，中心点y = 左上角y + 高度/2。）
+		FVector2D CenterPoint = BoxLeftTop + BoxSize * 0.5f;
+		// 将中心点归一化到0-1范围
+		float NormailizedX = FMath::Clamp(CenterPoint.X / WidgetSize.X, 0.0f, 1.0f);
+		float NormailizedY = FMath::Clamp(CenterPoint.Y / WidgetSize.Y, 0.0f, 1.0f);
+
+
 		// 更新选择框UI（如半透明矩形）
 		UpdateSelectionBox(BoxSize);
 	}
