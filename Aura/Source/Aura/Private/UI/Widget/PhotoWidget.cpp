2ca71cf23442432e669922e1f1c3a9ee20479e12
pxrM <49084047+pxrM@users.noreply.github.com>
2025-06-04 23:33:25 +0800

    Aura-p-3

--- Aura/Source/Aura/Private/UI/Widget/PhotoWidget.cpp
+++ Aura/Source/Aura/Private/UI/Widget/PhotoWidget.cpp
@@ -3,22 +3,53 @@
 
 #include "UI/Widget/PhotoWidget.h"
 
-FEventReply UPhotoWidget::OnTouchGesture(FGeometry MyGeometry, const FPointerEvent& GestureEvent)
+FEventReply UPhotoWidget::TouchGesture(FGeometry MyGeometry, const FPointerEvent& GestureEvent)
 {
-    return FEventReply();
+	FEventReply Reply;
+	Reply.NativeReply = FReply::Handled();
+	return Reply;
 }
 
-FEventReply UPhotoWidget::OnTouchStarted(FGeometry MyGeometry, const FPointerEvent& InTouchEvent)
+FEventReply UPhotoWidget::TouchStarted(FGeometry MyGeometry, const FPointerEvent& InTouchEvent)
 {
-    return FEventReply();
+	FEventReply Reply;
+	Reply.NativeReply = FReply::Handled();
+	SelectionStart = MyGeometry.AbsoluteToLocal(InTouchEvent.GetScreenSpacePosition());
+	bIsSelecting = true;
+	// 初始化选择框UI（如半透明矩形）
+	return Reply;
 }
 
-FEventReply UPhotoWidget::OnTouchMoved(FGeometry MyGeometry, const FPointerEvent& InTouchEvent)
+FEventReply UPhotoWidget::TouchMoved(FGeometry MyGeometry, const FPointerEvent& InTouchEvent)
 {
-    return FEventReply();
+	FEventReply Reply;
+	Reply.NativeReply = FReply::Handled();
+	if (bIsSelecting)
+	{
+		// 更新选择框的结束位置
+		SelectionEnd = MyGeometry.AbsoluteToLocal(InTouchEvent.GetScreenSpacePosition());
+		// 计算选择框的大小
+		FVector2D BoxSize = FVector2D(FMath::Abs(SelectionEnd.X - SelectionStart.X), FMath::Abs(SelectionEnd.Y - SelectionStart.Y));
+		// 更新选择框UI（如半透明矩形）
+		UpdateSelectionBox(BoxSize);
+	}
+	return Reply;
 }
 
-FEventReply UPhotoWidget::OnTouchEnded(FGeometry MyGeometry, const FPointerEvent& InTouchEvent)
+FEventReply UPhotoWidget::TouchEnded(FGeometry MyGeometry, const FPointerEvent& InTouchEvent)
 {
-    return FEventReply();
+	FEventReply Reply;
+	Reply.NativeReply = FReply::Handled();
+	if (bIsSelecting)
+	{
+		bIsSelecting = false;
+		// 选择结束，更新选择框UI
+		FIntRect CropArea(
+			FMath::Min(SelectionStart.X, SelectionEnd.X),
+			FMath::Min(SelectionStart.Y, SelectionEnd.Y),
+			FMath::Max(SelectionStart.X, SelectionEnd.X),
+			FMath::Max(SelectionStart.Y, SelectionEnd.Y)
+		);
+	}
+	return Reply;
 }
--- Aura/Source/Aura/Public/UI/Widget/PhotoWidget.h
+++ Aura/Source/Aura/Public/UI/Widget/PhotoWidget.h
@@ -15,20 +15,25 @@ class AURA_API UPhotoWidget : public UUserWidget
 	GENERATED_BODY()
 
 protected:
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCosmetic, Category = "Touch Input")
-	FEventReply OnTouchGesture(FGeometry MyGeometry, const FPointerEvent& GestureEvent);
+	FEventReply TouchGesture(FGeometry MyGeometry, const FPointerEvent& GestureEvent);
 
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCosmetic, Category = "Touch Input")
-	FEventReply OnTouchStarted(FGeometry MyGeometry, const FPointerEvent& InTouchEvent);
+	FEventReply TouchStarted(FGeometry MyGeometry, const FPointerEvent& InTouchEvent);
 
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCosmetic, Category = "Touch Input")
-	FEventReply OnTouchMoved(FGeometry MyGeometry, const FPointerEvent& InTouchEvent);
+	FEventReply TouchMoved(FGeometry MyGeometry, const FPointerEvent& InTouchEvent);
 
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCosmetic, Category = "Touch Input")
-	FEventReply OnTouchEnded(FGeometry MyGeometry, const FPointerEvent& InTouchEvent);
+	FEventReply TouchEnded(FGeometry MyGeometry, const FPointerEvent& InTouchEvent);
 
 public:
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
 	void BlueprintInitializeWidget();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void UpdateSelectionBox(FVector2D BoxSize);
+
+
+public:
+	FVector2D SelectionStart;
+	FVector2D SelectionEnd;
+	bool bIsSelecting = false;
 	
 };
